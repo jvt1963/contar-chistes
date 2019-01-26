e9e61cd011ad6b0c28c5a1c202bc2a3108f2f234
jvt1963 <39003362+jvt1963@users.noreply.github.com>
2019-01-27 00:48:52 +0100

    Update action-contar-chistes.py

--- action-contar-chistes.py
+++ action-contar-chistes.py
@@ -25,6 +25,7 @@ def intent_received(hermes, intent_message):
         sentence = extraer_chiste()
         
     else:
+        sentence = "Lo siento, no te he entendido"
         return
     
     hermes.publish_end_session(intent_message.session_id, sentence)
